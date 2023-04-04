621ac709e7b11e60be9e935e3ec8af1ffe5444ca
Preethyvijayan <preethivijayan27@gmail.com>
2023-04-04 16:29:41 +0530

    Constants

---
+++ Constants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Constants {
+	public static String CONFIGfILE="\\src\\main\\java\\Resources\\config.properties";
+	public static String TESTDATAFILE="\\src\\main\\java\\Resources\\TestData.xlsx";
+
+}
